b169d668b5ad90bb7fee66c2b2403fe655922875
tranthanhhai-59130561 <01674518584hai@gmail.com>
2020-04-17 19:29:04 +0700

    BaiTap2_StrateryPattern

--- TranThanhHai_59130561_StrateryPattern/src/BaiTap2/ThanhToanCOD.java
+++ TranThanhHai_59130561_StrateryPattern/src/BaiTap2/ThanhToanCOD.java
@@ -9,6 +9,19 @@ package BaiTap2;
  *
  * @author Admin
  */
-public class ThanhToanCOD {
+public class ThanhToanCOD implements IThanhToan{
+
+    @Override
+    public double thanhToan(int tienHang) {
+        if(tienHang > 2000000)
+        {
+            System.out.println("Khach hang duoc giam 2%: ");
+            return 0.02*tienHang;
+        }
+        else
+        {
+            return tienHang;
+        }
+    }
     
 }
