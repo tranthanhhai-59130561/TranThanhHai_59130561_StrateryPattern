4588a3e621bb9e5064f25125fa60d399473733f3
tranthanhhai-59130561 <01674518584hai@gmail.com>
2020-04-17 21:27:32 +0700

    BaiTap2_StrateryPattern

--- TranThanhHai_59130561_StrateryPattern/src/BaiTap2/Main2.java
+++ TranThanhHai_59130561_StrateryPattern/src/BaiTap2/Main2.java
@@ -19,7 +19,7 @@ public class Main2 {
     private static void inDS(ArrayList<HangHoa> dS){
         int s = 0;
         for (int i=0;i<dS.size();i++) {
-            System.out.println("--Ten hang:"+dS.get(i).getTenHH()+", Gia:"+dS.get(i).getGia()+", Mo ta:"+dS.get(i).getMoTa());
+            System.out.println("Ten hang hoa: "+"\n"+dS.get(i).getTenHH()+", Gia:"+dS.get(i).getGia()+", Mo ta:"+dS.get(i).getMoTa());
             s = s + dS.get(i).getGia();
         }
         System.out.println("Tien hang: " + s);
@@ -32,28 +32,27 @@ public class Main2 {
         HangHoa hH3 = new HangHoa("ram 16GB", 1900000, "Gia cao, dung luong cao");
         HangHoa hH4 = new HangHoa("Ram 32GB", 3900000, "Gia rat cao, dung luong rat cao");
         
+        //
         // Gio Hang 1
         GioHang gH1 = new GioHang();
-        gH1.setHinhThucTT(new ThanhToanCOD());
-        
+        gH1.setHinhThucTT(new ThanhToanCOD());       
         ArrayList <HangHoa> dS1 = new ArrayList<>();
         dS1.add(hH4);
         dS1.add(hH1);
-        inDS(dS1);
+        inDS(dS1);        
+        gH1.setHangHoa();       
         
-        gH1.setHangHoa();
-        System.out.println("Tong so tien gio hang 1: "+gH1.thanhToan());
-        
+        //
         // Gio Hang 2
         GioHang gH2 = new GioHang();
-        gH2.setHinhThucTT(new ThanhToanOnline());
-        
+        gH2.setHinhThucTT(new ThanhToanOnline());       
         ArrayList <HangHoa> dS2 = new ArrayList<>();
         dS2.add(hH3);
-        dS2.add(hH2);
-        inDS(dS2);
-        
+        dS2.add(hH2);        
+        //System.out.println("Tong so tien gio hang 2: "+gH2.thanhToan()); 
+        // Loi exeption ... red link ...        
+        inDS(dS2);        
         gH2.setHangHoa();
-        System.out.println("Tong so tien gio hang 2: "+gH2.thanhToan());        
+               
     }
 }
