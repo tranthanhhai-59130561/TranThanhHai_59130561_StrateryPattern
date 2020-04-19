0e34a2214d25f65cabfbcafd3a90534986cea045
tranthanhhai-59130561 <01674518584hai@gmail.com>
2020-04-19 11:25:44 +0700

    BaiTap3_StrateryPattern

--- TranThanhHai_59130561_StrateryPattern/src/BaiTap3/QLSV.java
+++ TranThanhHai_59130561_StrateryPattern/src/BaiTap3/QLSV.java
@@ -4,11 +4,22 @@
  * and open the template in the editor.
  */
 package BaiTap3;
-
+import java.util.ArrayList;
 /**
  *
  * @author Admin
  */
 public class QLSV {
+    private ArrayList<SinhVien> dsSV;
+    private ISoSanh<SinhVien> iSoSanh;
     
+    public void setDsSV(ArrayList<SinhVien> dsSV)
+    {
+        this.dsSV = dsSV;
+    }
+    
+    public void setISoSanh(ISoSanh<SinhVien> iSoSanh)
+    {
+        this.iSoSanh = iSoSanh;
+    }
 }
