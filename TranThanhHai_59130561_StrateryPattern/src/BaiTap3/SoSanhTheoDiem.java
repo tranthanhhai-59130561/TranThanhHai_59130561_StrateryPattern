ca63f79278a28bd84e36af6c6e2778ed1c266b90
tranthanhhai-59130561 <01674518584hai@gmail.com>
2020-04-19 11:41:41 +0700

    BaiTap3_StrateryPattern

--- TranThanhHai_59130561_StrateryPattern/src/BaiTap3/SoSanhTheoDiem.java
+++ TranThanhHai_59130561_StrateryPattern/src/BaiTap3/SoSanhTheoDiem.java
@@ -9,6 +9,21 @@ package BaiTap3;
  *
  * @author Admin
  */
-public class SoSanhTheoDiem {
+public class SoSanhTheoDiem implements ISoSanh<SinhVien> {
+
+    @Override
+    public int soSanh(SinhVien o1, SinhVien o2) {
+        if (o1.getDiemTB() < o2.getDiemTB())
+        {
+            return 1;
+        }
+        else
+        if (o1.getDiemTB() == o2.getDiemTB())
+        {
+            return 0;
+        }
+        else
+            return (-1);
+    }
     
 }
